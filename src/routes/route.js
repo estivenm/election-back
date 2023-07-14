6ef8caa9a8a832c263883133faf591115d57f436
Estiven Mazo <juan.mazo@imagemaker.com>
2023-07-13 22:29:50 -0500

    fix: typo

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,7 +5,7 @@ const BASE_PATH = env.BASE_PATH
 
 const routeConfig = (app) => {
   app.use(`${BASE_PATH}/elections`, electionRoutes)
-  app.use(`${BASE_PATH}/docs1`, swaggerDocs)
+  app.use(`${BASE_PATH}/docs`, swaggerDocs)
 }
 
 export { routeConfig }
