0766c46d22fd19ad644dac0e3636a0bd5394061d
Estiven Mazo <juan.mazo@imagemaker.com>
2023-07-13 22:36:08 -0500

    feat: add get test

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,6 +4,9 @@ import { electionRoutes } from './election.route.js'
 const BASE_PATH = env.BASE_PATH
 
 const routeConfig = (app) => {
+  app.get('/', (req, res) => {
+    res.send('¡Hi!')
+  })
   app.use(`${BASE_PATH}/elections`, electionRoutes)
   app.use(`${BASE_PATH}/docs`, swaggerDocs)
 }
